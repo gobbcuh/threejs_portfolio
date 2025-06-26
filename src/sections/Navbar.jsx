fa61d227370311ad381cc78e453dc132b1bcb3a4
jasmin <jajalorino@gmail.com>
2025-06-26 13:02:38 +0800

    added button

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen((prevIsOpen) => !prevIsOpen);
+    }
+
     return (
         <header className = "fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className = "max-w-6xl mx-auto">
@@ -8,8 +14,8 @@ const Navbar = () => {
                     <a href = "/" className = "text-neutral-400 font-bold text-xl hover:text-white transition-colors">
                         Jasmin
                     </a>
-                    <button>
-                        <img src = {} />
+                    <button onClick = {toggleMenu} className= "text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label = "Toggle menu">
+                        <img src = {isOpen ? "/assets/close.svg" : "assets/menu.svg"} alt = "toggle" className = "w-6 h-6" />
                     </button>
                 </div>
             </div>
