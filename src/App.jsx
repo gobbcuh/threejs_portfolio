2171771795233c1b227cae684d7f017d78bb7368
jasmin <jajalorino@gmail.com>
2025-06-26 12:51:51 +0800

    removed public folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-        <main className = "max-w-7xl mx-auto">
+        <main className = "max-w-6xl mx-auto">
             <h1 className = "text-2xl text-white underline">Hello Three.js!</h1>
         </main>
     )
