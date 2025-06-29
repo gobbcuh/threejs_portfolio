528ef36e4aeaebd8efe32b04aa22c35a5036148a
jasmin <jajalorino@gmail.com>
2025-06-29 13:14:52 +0800

    added constants folder

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -2,9 +2,15 @@ import React, {useState} from 'react'
 
 const NavItems = () => {
     return (
-        <div>
-
-        </div>
+        <ul className = "nav-ul">
+            {["Home", "About", "Projects", "Contact"].map((item, index) => (
+                <li key = {index} className = "nav-li">
+                    <a href = "/" className = "nav-li_a">
+                        {item}
+                    </a>
+                </li>
+            ))}
+        </ul>
     )
 }
 
