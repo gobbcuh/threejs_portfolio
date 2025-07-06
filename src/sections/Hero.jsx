5ae9c2ffabe665770fe20f6f33ef11edb79c5efe
jasmin <jajalorino@gmail.com>
2025-07-06 23:52:54 +0800

    edited content

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
     return (
         <section className = "min-h-screen w-full flex flex-col relative">
             <div className = "w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-                <p className= "sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I'm Jasmin <span className = "waving-hand">💗</span></p>
+                <p className= "sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Jasmin <span className = "waving-hand">💗</span></p>
                 <p className = "hero_tag text-gray_gradient">Building Products & Brands</p>
             </div>
 
