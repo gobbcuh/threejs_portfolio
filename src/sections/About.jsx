c12746ef8098ac79fa61d3ec437c08f528a9871d
jasmin <jajalorino@gmail.com>
2025-07-07 15:43:52 +0800

    installed gl

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className = "c-space my-20">
+            <div className = "grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+                <div className="col-span-1 xl: row-span-3">
+            <div className = "grid-container">
+                <img src = "/assets/grid1.png" alt="grid-1" className = "w-full sm:h-[276px] h-fit object-contain"/>
+                <div>
+                    <p className = "grid-headtext">Hi, I am Jasmin</p>
+                    <p className = "grid-subtext">Currently a Computer Science student, exploring both frontend and backend development by learning new technologies and building projects.</p>
+                </div>
+            </div>
+        </div>
+
+                <div className="col-span-1 xl: row-span-3">
+                    <div className = "grid-container">
+                        <img src = "/assets/grid2.png" alt="grid-2" className = "w-full sm:h-[276px] h-fit object-contain"/>
+                        <div>
+                            <p className = "grid-headtext">Tech Stack</p>
+                            <p className = "grid-subtext">I have experience in a wide range of technologies, including JavaScript, React, Node.js, Express.js, and MongoDB.</p>
+                        </div>
+                    </div>
+                </div>
+
+                    <div className="col-span-1 xl: row-span-4">
+                        <div className = "grid-container">
+                            <div className = "rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+
+                            </div>
+                        </div>
+                    </div>
+
+            </div>
+        </section>
     )
 }
 export default About
