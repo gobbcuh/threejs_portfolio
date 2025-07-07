ac5e6df47c66a110efa46c57a958e0656628ade8
jasmin <jajalorino@gmail.com>
2025-07-07 16:15:04 +0800

    passion section + added loc

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,5 @@
 import Globe from "react-globe.gl";
+import Button from "../components/Button.jsx";
 
 const About = () => {
     return (
@@ -36,10 +37,36 @@ const About = () => {
                                     showGraticules
                                     globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
                                     bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                                    labelsData = {[{
+                                        lat: 14.445, lng: 120.907,
+                                        text: "I'm here!",
+                                        color: 'white',
+                                        size: 20,
+                                    }]}
                                 />
+                            </div>
+
+                            <div>
+                                <p className = "grid-headtext">Location</p>
+                                <p className = "grid-subtext">I'm currently based in the Philippines.</p>
+                                <Button name = "Contact Me" isBeam containerClass = "w-full mt-10"/>
                             </div>
                         </div>
                     </div>
+
+                        <div className = "col-span-2 xl: row-span-3">
+                            <div className = "grid-container">
+                                <img src = "/assets/grid3.png" alt="grid-3" className = "w-full sm:h-[266px] h-fit object-contain" />
+                                <div>
+                                    <p className = "grid-headtext">My Passion for Coding</p>
+                                    <p className = "grid-subtext">Coding allows me to build things that satisfy my curiosity and interest, while also solving real-world problems.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className>
+
+                        </div>
 
             </div>
         </section>
