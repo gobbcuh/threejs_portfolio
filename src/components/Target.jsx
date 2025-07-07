de6e0d7f43f6430914f20bbe4d9b7506dd52c6e7
jasmin <jajalorino@gmail.com>
2025-07-07 08:50:06 +0800

    fixed target for rendering

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import { useRef } from 'react'
 
-const Target = () => {
+const Target = (props) => {
+    const targetRef = useRef();
+    const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf')
+
     return (
-        <div>Target</div>
+        <mesh {...props} ref={targetRef}>
+            <primitive object={scene} />
+        </mesh>
     )
 }
 export default Target
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,8 +17,8 @@ const Hero = () => {
     return (
         <section className = "min-h-screen w-full flex flex-col relative">
             <div className = "w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-                <p className= "sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I'm Jasmin <span className = "waving-hand">💗</span></p>
-                <p className = "hero_tag text-gray_gradient">Building Products & Brands</p>
+                <p className= "sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Jasmin <span className = "waving-hand">💗</span></p>
+                <p className = "hero_tag text-gray_gradient">CS Freshman Student</p>
             </div>
 
             <div className= "w-full h-full absolute inset-0">
@@ -32,9 +32,8 @@ const Hero = () => {
                         scale={sizes.deskScale}
                         rotation={[0, -Math.PI, 0]}
                     />
-
                         <group>
-                            <Target position={sizes.targetPosition}/>
+                            <Target posiition={sizes.targetPosition} />
                         </group>
 
                         <ambientLight intensity={1} />
